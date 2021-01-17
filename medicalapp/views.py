222943dea150a84a01644f38894cb3f32ae98a84
Shikha Gupta <68690935+Shikha291@users.noreply.github.com>
2021-01-17 12:59:34 +0530

    Added twilio part

--- medicalapp/views.py
+++ medicalapp/views.py
@@ -11,6 +11,22 @@ def register(request):
         number = request.POST['number']
         print(name)
         print(number)
+    from twilio.rest import Client
+    import os
+
+    # Your Account Sid and Auth Token from twilio.com/console
+    # and set the environment variables. See http://twil.io/secure
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="Hey!"+name+ "You are registered for vaccination..",
+        from_='+14342904181',
+        to='+919205201181 , +917404349242'
+    )
+
+    print(message.sid)
 
     return render(request, "register.html")
 
